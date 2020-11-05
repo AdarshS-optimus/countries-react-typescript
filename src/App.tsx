9835ddc28030b3a4dc2155ae080b2735d16ac90e
adarshs-optimus <adarsh.srivastava@optimusinfo.com>
2020-11-05 16:58:53 +0530

    First commit ever

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import HomeComponent from './components/home/home.component';
+import RegionWiseComponent from './components/region-wise/region-wise.component';
+import CountryProfileComponent from './components/country-profile/country-profile.component';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Container, Card } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+          <Card className="header">
+              <Card.Body className="header-text"><img src="https://img.icons8.com/color/48/globe.png" /> Where in the world ?</Card.Body>
+          </Card>
+      </Container>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomeComponent} />
+          <Route exact path="/region/:region" component={RegionWiseComponent} />
+          <Route path="/:alpha3Code" component={CountryProfileComponent} />
+        </Switch>
+      </Router>
     </div>
   );
 }
